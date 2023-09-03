7ac4f0bcfab37622cc0d925a30ded029f9429ea3
syket <syketweb@gmail.com>
2023-09-03 23:36:59 +0600

    skip string function modified for without empty string

--- string.js
+++ string.js
@@ -1,6 +1,7 @@
 let s = "syket bhattachergee";
 let newString = "";
 
+// with pass the new empty string
 const skipString = (s, target, newString) => {
   //   console.log(newString);
   if (s.length === 0) {
@@ -16,4 +17,24 @@ const skipString = (s, target, newString) => {
     skipString(s.substring(1), target, (newString += ch));
   }
 };
-skipString(s, "e", newString);
+// skipString(s, "e", newString);
+
+// without pass the empty string
+
+const skipString2 = (s, target) => {
+  if (s.length === 0) {
+    return s;
+  }
+
+  let ch = s.charAt(0);
+
+  if (ch === target) {
+    return skipString2(s.substring(1), target);
+  } else {
+    return (ch += skipString2(s.substring(1), target));
+  }
+};
+
+const s2 = "syket bhattachergee";
+const res = skipString2(s2, "e");
+console.log(res);
